2f7e8cfa08f6c8b1d83fa303c6794459813dd55a
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-12 14:06:54 +0000

    app handling post requests with code

--- app/app.js
+++ app/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const utils = require("./utils");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = express.Router();
 
-app.get('/compile', function (req, res) {
-  res.send('Hello World!');
+const port = process.env.PORT || 3000;
+
+// this will let us get the data from a POST
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.post('/compile', function(req, res) {
+  const langId = req.body.langId;
+  const code = req.body.code;
+  const stdin = req.body.stdin;
+  res.json({
+    langId: langId,
+    code: code,
+    stdIn: stdIn
+  });
 });
 
-app.listen(3000, function () {
-  const randomString = utils.randomString(10);
-  console.log(randomString);
+app.use('/api', router);
+
+app.listen(port, function () {
   console.log('App listening on port 3000!')
 });
