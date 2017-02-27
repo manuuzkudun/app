443637ab5e36d1eed3cc23600d8514b9a7fee889
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-27 17:12:12 +0000

    parser added

--- app/sandbox.js
+++ app/sandbox.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const exec = require('child_process').exec;
+const parser = require('./parser');
 
 module.exports.compile = function (code, test, success) {
   fs.writeFile("./temp/code.rb", code, function(err) {
@@ -7,7 +8,7 @@ module.exports.compile = function (code, test, success) {
     fs.writeFile("./temp/test.rb", test, function(err) {
       if (err) return console.log(err);
       exec("ruby ./temp/test.rb", function(error, stdout, stderr) {
-        success(stdout, stderr);
+        success(parser.parseStdOut(stdout), parser.parseStdError(stderr));
         console.log(stdout);
       });
     });
